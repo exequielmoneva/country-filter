31103a6e3b4daa1ad9be75dab38e6c42dd13c96b
exequielmoneva <exequielmoneva@gmail.com>
2021-01-13 15:26:16 -0300

    Swagger added

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, send_from_directory
 
 from schemas.schemas import CountrySchema
 from services.dataset_service import DatasetService
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 preprocessing = DatasetService()
 schema = CountrySchema(many=True)
-
 
 @app.route('/')
 def hello_world():
     return 'API is running'
 
 
-@app.route('/country', methods=['POST'])
-def country_filter():
-    value = float(request.json.get("value"))
-    countries = preprocessing.pd_service(value)
-    return jsonify(schema.dump(countries))
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.yaml /'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "countryFilter"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+
+@app.route('/country/<satisfaction_index>')
+def country_filter(satisfaction_index):
+    try:
+        countries = preprocessing.pd_service(float(satisfaction_index))
+        return make_response(jsonify({"countries": schema.dump(countries)}),200)
+    except ValueError as e:
+        return make_response(jsonify({'error': str(e)}),400)
 
 
 if __name__ == '__main__':
